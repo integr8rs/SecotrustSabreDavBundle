5dc6db15373d9ef6ef06db6925fa1ca83728c0b0
Zombaya <Zombaya@users.noreply.github.com>
2019-12-04 16:42:26 +0100

    Add compatibility for symfony 5.0

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -24,8 +24,13 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('secotrust_sabre_dav');
+        $treeBuilder = new TreeBuilder('secotrust_sabre_dav');
+        if (\method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            // BC layer for symfony/config 4.1 and older
+            $rootNode = $treeBuilder->root('secotrust_sabre_dav');
+        }
 
         $default_base_uri = '/app_dev.php/remote';
 
